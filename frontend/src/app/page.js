0ed3a525e1170cef36baf91ac39435061d5afda0
Nikk434 <nskshirsagar434@gmail.com>
2025-08-23 15:36:07 +0530

    minor changes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,26 @@
+'use client'
 import LoginPage from "./auth/login/page";
 // import TreatmentEditor from "./components/tiptap";
 // import Tiptap from "./components/tiptap";
+import MatchSettings from "./components/MatchStart";
 
 export default function Home() {
+  const handleStartMatch = (settings) => {
+    console.log('Match settings:', settings)
+    // Navigate to the actual match/editor
+  }
+
+  const handleCancel = () => {
+    // Navigate back or close modal
+  }
+
+  
   return (
-    <LoginPage/>
+    // <LoginPage />
+    <MatchSettings
+    onStartMatch={handleStartMatch}
+    onCancel={handleCancel}
+  />
     // ,    
     // <TreatmentEditor/>
   );
