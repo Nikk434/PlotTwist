fffdf0657fee71ed2e2ec8c7ebe639a3d2516edf
Nikk434 <nskshirsagar434@gmail.com>
2025-10-26 12:31:29 +0530

    update - indexed all the id fields for better quering

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -17,5 +17,12 @@ reminder_collection = db["reminders"]
 subscription_collection = db["subscription"]
 match_setting = db["match"]
 story_collection = db["script"]
+async def init_indexes():
+    # await user_collection.create_index("email", unique=True)
+    await match_setting.create_index("hostedBy.userId")
+    await story_collection.create_index("match_id")
+    await story_collection.create_index("user_id")
+    await story_collection.create_index("feedback.fromUserId")
+
 # Optional: Ensure email is unique
 user_collection.create_index([("email", ASCENDING)], unique=True)
